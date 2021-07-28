b2f1b2b309c49657c0165e4b190d652907a318a7
Thushar <thushar.work@gmail.com>
2021-07-28 14:31:25 +0530

    Partial header for login, register pages

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>
+        {' '}
+        <img src="/images/logo.svg" alt="A logo" width="50px" /> MIXD{' '}
+      </h1>
+    </header>
+  );
+}
