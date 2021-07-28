9fe8d25a71dd2cb5ce9bf2947817f94a43f72b22
Thushar <thushar.work@gmail.com>
2021-07-28 14:32:55 +0530

    Article cards to be displayed in a grid

---
+++ components/ArticleCards.js
@@ -0,0 +1,56 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import router from 'next/router';
+import { useState } from 'react';
+import Cookies from 'universal-cookie';
+import Date from './date';
+
+export default function ArticleCards({ article, handleDelete, errorMessage }) {
+  const editArticle = (e) => {
+    e.preventDefault();
+    router.push(`/user/edit/${article.id}`);
+  };
+
+  const deleteArticle = async (e) => {
+    e.preventDefault();
+    handleDelete(article.id, article.title);
+  };
+
+  const imagePath = `/images/uploads/${article.id}.jpeg`;
+  return (
+    <div className="article-section">
+      <div>
+        <div className="img-container">
+          <Image src={imagePath} height={256} width={300} />
+        </div>
+
+        <h3>
+          {' '}
+          <Link href={`/posts/${article.id}`}>
+            <a>{article.title} </a>
+          </Link>
+        </h3>
+      </div>
+      <div>
+        <span>
+          <Date dateString={article.date} />
+        </span>
+        &nbsp;|&nbsp;
+        <span> {article.userTable.name} </span>
+      </div>
+      {localStorage.getItem('isLogged') === 'true' ? (
+        <div>
+          <button className="span-btn" type="button" name="edit" onClick={(e) => editArticle(e)}>
+            <span> Edit </span>
+          </button>
+          |
+          <button className="span-btn" type="button" name="delete" onClick={(e) => deleteArticle(e)}>
+            <span> Delete </span>
+          </button>
+        </div>
+      ) : (
+        ''
+      )}
+    </div>
+  );
+}
