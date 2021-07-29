d2cdbe4cb17af3d6b4051e95a3f414c335a72886
Thushar <thushar.work@gmail.com>
2021-07-29 07:18:32 +0530

    Add Head meta- tag

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,25 @@
+import Head from 'next/head';
+
 export default function Header() {
   return (
-    <header>
-      <h1>
-        {' '}
-        <img src="/images/logo.svg" alt="A logo" width="50px" /> MIXD{' '}
-      </h1>
-    </header>
+    <>
+      <Head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" />
+        <title> Mixd </title>
+        <link rel="icon" href="/images/logo.svg" type="image/icon type" />
+      </Head>
+      <header>
+        <h1>
+          {' '}
+          <img src="/images/logo.svg" alt="A logo" width="50px" /> MIXD{' '}
+        </h1>
+      </header>
+    </>
   );
 }
